85bf8b75f82a75bb5aaaa63efbc650d75a558097
Raphael Nacache <rnacache@g.ucla.edu>
2018-04-24 11:45:37 -0700

    Homework, midterm, and final percentage.

--- GradeCalc/GradeCalc/GradeCalc.cpp
+++ GradeCalc/GradeCalc/GradeCalc.cpp
@@ -5,5 +5,20 @@ using namespace std;
 
 int main()
 {
-	cout << "hello world" << endl;
+	cout << "Welcome to Grade Calculator!" << endl;
+
+	int homework_percent;
+	int midterm_percent;
+	int final_percent;
+
+	cout << "Please list the percentage of each section." << endl;
+
+	cout << "Homework %: ";
+	cin >> homework_percent;
+
+	cout << "Midterm %: ";
+	cin >> midterm_percent;
+
+	cout << "Final %: ";
+	cin >> final_percent;
 }
