0198954ab82f58edc892f3d2b65fa4088b28639c
Raphael Nacache <rnacache@g.ucla.edu>
2018-04-25 11:20:05 -0700

    Grades and percentage reported

--- GradeCalc/GradeCalc/GradeCalc.cpp
+++ GradeCalc/GradeCalc/GradeCalc.cpp
@@ -10,6 +10,9 @@ int main()
 	int homework_percent;
 	int midterm_percent;
 	int final_percent;
+	double homework_grade;
+	double midterm_grade;
+	double final_grade;
 
 	cout << "Please list the percentage of each section." << endl;
 
@@ -21,4 +24,15 @@ int main()
 
 	cout << "Final %: ";
 	cin >> final_percent;
+
+	cout << "Now enter your grades." << endl;
+
+	cout << "Homework: ";
+	cin >> homework_grade;
+
+	cout << "Midterm: ";
+	cin >> midterm_grade;
+
+	cout << "Final: ";
+	cin >> final_grade;
 }
